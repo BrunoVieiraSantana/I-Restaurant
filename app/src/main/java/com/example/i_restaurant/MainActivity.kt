98ebc95fc5438348e119bb60915b41e5d3e4eb2d
BrunoVieiraSantana <brunonewdev@gmail.com>
2024-03-20 11:24:13 -0300

    Start 04

--- app/src/main/java/com/example/i_restaurant/MainActivity.kt
+++ app/src/main/java/com/example/i_restaurant/MainActivity.kt
@@ -1,16 +1,34 @@
 package com.example.i_restaurant
 
+import android.app.Activity
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.i_restaurant.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    var clicked = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.btEntradas.setOnClickListener{
+            clicked = true
+            if (clicked){
+                binding.btEntradas.setBackgroundResource(R.drawable.bg_button_enable)
+                binding.btEntradas.setTextColor(Color.WHITE)
+            }
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
