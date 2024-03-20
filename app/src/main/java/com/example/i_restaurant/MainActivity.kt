b5ca06dfd9a6e33266846fd9a30db4575d54150c
BrunoVieiraSantana <brunonewdev@gmail.com>
2024-03-20 18:21:27 -0300

    commit work

--- app/src/main/java/com/example/i_restaurant/MainActivity.kt
+++ app/src/main/java/com/example/i_restaurant/MainActivity.kt
@@ -26,7 +26,65 @@ class MainActivity : AppCompatActivity() {
             if (clicked){
                 binding.btEntradas.setBackgroundResource(R.drawable.bg_button_enable)
                 binding.btEntradas.setTextColor(Color.WHITE)
+                binding.btPrincipais.setBackgroundResource(R.drawable.bg_button_disabled)
+                binding.btPrincipais.setTextColor(Color.dark_gray)
+                binding.btBebidas.setBackgroundResource(R.drawable.bg_button_disabled)
+                binding.btBebidas.setTextColor(Color.dark_gray)
+                binding.btSobremesas.setBackgroundResource(R.drawable.bg_button_disabled)
+                binding.btSobremesas.setTextColor(Color.dark_gray)
+                binding.recyclerViewProducts.visibility = View.INVISIBLE
+                binding.txtListTitle.text = "Entradas"
+                }
             }
+
+        binding.btPrincipais.setOnClickListener{
+            clicked = true
+            if (clicked){
+                binding.btPrincipais.setBackgroundResource(R.drawable.bg_button_enable)
+                binding.btPrincipais.setTextColor(Color.WHITE)
+                binding.btEntradas.setBackgroundResource(R.drawable.bg_button_disabled)
+                binding.btEntradas.setTextColor(Color.dark_gray)
+                binding.btBebidas.setBackgroundResource(R.drawable.bg_button_disabled)
+                binding.btBebidas.setTextColor(Color.dark_gray)
+                binding.btSobremesas.setBackgroundResource(R.drawable.bg_button_disabled)
+                binding.btSobremesas.setTextColor(Color.dark_gray)
+                binding.recyclerViewProducts.visibility = View.INVISIBLE
+                binding.txtListTitle.text = "Principais"
+            }
+        }
+
+        binding.btBebidas.setOnClickListener{
+            clicked = true
+            if (clicked){
+                binding.btBebidas.setBackgroundResource(R.drawable.bg_button_enable)
+                binding.btBebidas.setTextColor(Color.WHITE)
+                binding.btPrincipais.setBackgroundResource(R.drawable.bg_button_disabled)
+                binding.btPrincipais.setTextColor(Color.dark_gray)
+                binding.btEntradas.setBackgroundResource(R.drawable.bg_button_disabled)
+                binding.btEntradas.setTextColor(Color.dark_gray)
+                binding.btSobremesas.setBackgroundResource(R.drawable.bg_button_disabled)
+                binding.btSobremesas.setTextColor(Color.dark_gray)
+                binding.recyclerViewProducts.visibility = View.INVISIBLE
+                binding.txtListTitle.text = "Bebidas"
+            }
+        }
+
+        binding.btSobremesas.setOnClickListener{
+            clicked = true
+            if (clicked){
+                binding.btSobremesas.setBackgroundResource(R.drawable.bg_button_enable)
+                binding.btSobremesas.setTextColor(Color.WHITE)
+                binding.btBebidas.setBackgroundResource(R.drawable.bg_button_disable)
+                binding.btBebidas.setTextColor(Color.dark_gray)
+                binding.btPrincipais.setBackgroundResource(R.drawable.bg_button_disable)
+                binding.btPrincipais.setTextColor(Color.dark_gray)
+                binding.btEntradas.setBackgroundResource(R.drawable.bg_button_disable)
+                binding.btEntradas.setTextColor(Color.dark_gray)
+                binding.recyclerViewProducts.visibility = View.INVISIBLE
+                binding.txtListTitle.text = "Sobremesas"
+            }
+        }
+
         }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
