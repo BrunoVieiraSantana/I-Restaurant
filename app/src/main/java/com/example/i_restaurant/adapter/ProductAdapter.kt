a8779fa8e801163522cb7ae9f4d8646c2418132a
BrunoVieiraSantana <brunonewdev@gmail.com>
2024-03-21 09:12:14 -0300

    Start 09

--- app/src/main/java/com/example/i_restaurant/adapter/ProductAdapter.kt
+++ app/src/main/java/com/example/i_restaurant/adapter/ProductAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.i_restaurant.adapter
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.i_restaurant.databinding.ProductItemBinding
@@ -10,18 +11,21 @@ class ProductAdapter(private val context: Context, private val productList: Muta
     RecyclerView.Adapter<ProductAdapter.ProductViewHolder>() {
 
     inner class ProductViewHolder(binding: ProductItemBinding): RecyclerView.ViewHolder(binding.root){
-
+        val imgProduct = binding.imgProduct
+        val name = binding.txtName
+        val price = binding.txtPrice
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
-        TODO("Not yet implemented")
+        val listItem = ProductItemBinding.inflate(LayoutInflater.from(context), parent,false)
+        return ProductViewHolder(listItem)
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount() = productList.size
 
     override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.imgProduct.setBackgroundResource(productList[position].imgProduct)
+        holder.name.text = productList[position].name
+        holder.price.text = productList[position].price
     }
 }
---
+++ app/src/main/java/com/example/i_restaurant/listitems/Products.kt
@@ -0,0 +1,20 @@
+package com.example.i_restaurant.listitems
+
+import com.example.i_restaurant.model.Product
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class Products {
+
+    private val _productList = MutableStateFlow<MutableList<Product>>(mutableListOf())
+    private val productListFlow: StateFlow<MutableList<Product>> = _productList
+
+    fun getProducts(): Flow<MutableList<Product>> {
+        val productList: MutableList<Product> = mutableListOf(
+            Product(
+                imgProduct = 
+            )
+        )
+    }
+}
