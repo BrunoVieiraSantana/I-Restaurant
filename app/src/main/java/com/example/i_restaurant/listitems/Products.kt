1c275c3a597e37ab48687e8f966dc3f517cd3f66
BrunoVieiraSantana <brunonewdev@gmail.com>
2024-03-21 09:21:50 -0300

    Start 10

--- app/src/main/java/com/example/i_restaurant/listitems/Products.kt
+++ app/src/main/java/com/example/i_restaurant/listitems/Products.kt
@@ -1,5 +1,6 @@
 package com.example.i_restaurant.listitems
 
+import com.example.i_restaurant.R
 import com.example.i_restaurant.model.Product
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -10,11 +11,30 @@ class Products {
     private val _productList = MutableStateFlow<MutableList<Product>>(mutableListOf())
     private val productListFlow: StateFlow<MutableList<Product>> = _productList
 
-    fun getProducts(): Flow<MutableList<Product>> {
+    fun getProducts(): Flow<MutableList<Product>>{
         val productList: MutableList<Product> = mutableListOf(
             Product(
-                imgProduct = 
+                imgProduct = R.drawable.cheese_pizza,
+                name = "Chesse Pizza",
+                price = "10.20"
+            ),
+            Product(
+                imgProduct = R.drawable.mixed_pizza,
+                name = "Mixed Pizza",
+                price = "10.20"
+            ),
+            Product(
+                imgProduct = R.drawable.classic_pizza,
+                name = "Classic Pizza",
+                price = "10.20"
+            ),
+            Product(
+                imgProduct = R.drawable.salmon_pizza,
+                name = "Salmon Pizza",
+                price = "10.20"
             )
         )
-    }
+        _productList.value = productList
+        return productListFlow
+     }
 }
