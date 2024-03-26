700346606bb9406c054bbfb52ff6aa54bbb19330
BrunoVieiraSantana <brunonewdev@gmail.com>
2024-03-26 10:48:21 -0300

    07 commit

--- app/src/main/java/com/example/i_restaurant/Table.kt
+++ app/src/main/java/com/example/i_restaurant/Table.kt
@@ -1,20 +1,24 @@
 package com.example.i_restaurant
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.i_restaurant.databinding.ActivityTableBinding
 
 class Table : AppCompatActivity() {
+
+    private lateinit var binding: ActivityTableBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_table)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityTableBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonContinuar.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
     }
 }
